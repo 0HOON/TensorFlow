6dda37cf60391989561b656fcecd39afc280e754
unknown <yhoonyh@gmail.com>
2019-01-10 23:29:15 +0900

    lec 10-1,2 ReLU and initialization

--- ML lab 09/NNforMNISTtest.py
+++ ML lab 09/NNforMNISTtest.py
@@ -13,46 +13,35 @@ X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, nb_classes])
 
 with tf.name_scope("layer1") as scope:
-    W1 = tf.Variable(tf.random_uniform([784, 256], minval = 10, maxval = 11, dtype = tf.float32)/np.sqrt(784/2))
-   #W1 = tf.Variable(tf.random_normal([784,256]))
+    W1 = tf.Variable(tf.random_uniform([784, 256], minval = 10, maxval = 11, dtype = tf.float32)/np.sqrt(10/2))
     b1 = tf.Variable(tf.random_normal([256]))
     layer1 = tf.nn.relu(tf.matmul(X,W1) + b1)
-    print(W1)
+   
 
-    w1_hist = tf.summary.histogram("weight1", W1)
-    b1_hist = tf.summary.histogram("bias1", b1)
-    layer_hist = tf.summary.histogram("layer1", layer1)
+
 
 with tf.name_scope("layer2") as scope:
-    W2 = tf.Variable(tf.random_uniform([256, 10], minval = 10, maxval = 11, dtype = tf.float32)/np.sqrt(256/2))
-    #W2 = tf.Variable(tf.random_normal([256, 10]))
+    W2 = tf.Variable(tf.random_uniform([256, 10], minval = 10, maxval = 100, dtype = tf.float32)/np.sqrt(100/2))
     b2 = tf.Variable(tf.random_normal([10]))
     layer2 = tf.nn.relu(tf.matmul(layer1,W2) + b2)
 
-    w2_hist = tf.summary.histogram("weight2", W2)
-    b2_hist = tf.summary.histogram("bias2", b2)
-    layer2_hist = tf.summary.histogram("layer2", layer2)
+
 
 with tf.name_scope("hypothesis") as scope:
     W3 = tf.Variable(tf.random_uniform([10,nb_classes], minval = 10, maxval = 11, dtype = tf.float32)/np.sqrt(10/2))
-    #W3 = tf.Variable(tf.random_normal([10, nb_classes]))
     b3 = tf.Variable(tf.random_normal([nb_classes]))
+    #hypothesis = tf.nn.softmax(tf.matmul(layer2,W3) + b3)
     hypothesis = tf.matmul(layer2,W3) + b3
 
-    w3_hist = tf.summary.histogram("weight3", W3)
-    b3_hist = tf.summary.histogram("bias3", b3)
-    hypothesis_hist = tf.summary.histogram("hypothesis", hypothesis)
-
-
+#cost = -tf.reduce_mean(tf.reduce_sum(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 -hypothesis) , axis = 1))
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = hypothesis, labels = Y))
 train = tf.train.AdamOptimizer(learning_rate = 0.01).minimize(cost)
 cost_summ = tf.summary.scalar("cost", cost)
 
 is_correct = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-accuracy_summ = tf.summary.scalar("accuraccy", accuracy)
 
-summary = tf.summary.merge_all()
+
 
 training_epochs = 15
 batch_size = 100
@@ -69,9 +58,9 @@ with tf.Session() as sess:
 
         for i in range(total_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            c, s, _ = sess.run([cost, summary, train], feed_dict = {X: batch_xs, Y: batch_ys})
+            c, _ = sess.run([cost, train], feed_dict = {X: batch_xs, Y: batch_ys})
             avg_cost += c / total_batch
-            writer.add_summary(s, global_step = epoch * total_batch + i)
+            
         
         print('Epoch: ', '%04d' % (epoch + 1), 'cost = ', '{:.9f}'.format(avg_cost))
     
